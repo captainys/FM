f054ee24f6e77c2d1528c60797f27a39211cdad6
Soji Yamakawa <soji@andrew.cmu.edu>
2022-05-09 22:26:07 -0400

    Re-instated BOOTSECT.  Also added sample script for making SCSI/Internal dual boot discs.

---
+++ TOWNS/IPL/UTIL/sample_rayxanber_bootsect.py
@@ -0,0 +1,7 @@
+import sys
+import instbincueipl
+
+
+
+if __name__=="__main__":
+	instbincueipl.main([sys.argv[0],"C:/d/townsiso/RAYXANBER.CUE","C:/d/townsiso/RAYXANBER_SCSI.CUE","../BOOTSECT/BOOTSECT.BIN"])
---
+++ TOWNS/IPL/UTIL/sample_townsosv21l31_bootsect.py
@@ -0,0 +1,7 @@
+import sys
+import instbincueipl
+
+
+
+if __name__=="__main__":
+	instbincueipl.main([sys.argv[0],"C:/d/townsiso/TownsOSV2.1L31.CUE","C:/d/townsiso/TownsOSV2.1L31_SCSI.CUE","../BOOTSECT/BOOTSECT.BIN"])
