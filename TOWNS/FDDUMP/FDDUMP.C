069103e7ffef5e711a1bb9045fe8a7b7b71ceada
Soji Yamakawa <soji@andrew.cmu.edu>
2022-11-04 23:05:18 -0400

    Working on FDDUMP.

--- TOWNS/FDDUMP/FDDUMP.C
+++ TOWNS/FDDUMP/FDDUMP.C
@@ -1,5 +1,28 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <dos.h>
+#include <conio.h>
+#include <signal.h>
+
+// Output Data Data Format (.TD1)
+// Begin Disk
+// 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 (16 bytes)
+// Begin Track
+// 01 00 cc hh 00 00 00 00 00 00 00 00 00 00 00 00 (16 bytes)
+// ID Mark
+// 02 00 cc hh rr nn <CRC> 00 00 00 00 00 00 00 00
+// 02 00 cc hh rr nn <CRC> 00 00 00 00 00 00 00 00
+// 02 00 cc hh rr nn <CRC> 00 00 00 00 00 00 00 00
+//     :
+// Data
+// 03 00 cc hh rr nn st 00 00 00 00 00 <Real Size>
+// (Bytes padded to 16*N bytes)
+// Track Read
+// 04 00 00 00 00 00 00 00 00 00 00 00 <Real Size>
+// (Bytes padded to 16*N bytes)
+
+// Next Track
+
 
 #define FDC_INT	0x46
 
@@ -12,9 +35,33 @@ void interrupt Handle_INT46H(void)
 	INT46_DID_COME_IN=1;
 }
 
+void CtrlC(int err)
+{
+	_dos_setvect(FDC_INT,Default_INT46H_Handler);
+	printf("Intercepted Ctrl+C\n");
+	exit(1);
+}
+
+
+struct CommandParameterInfo
+{
+};
+
+void ReadDisk(struct CommandParameterInfo *cpi)
+{
+}
+
+unsigned char FreeRunTimerAvailable(void)
+{
+}
+
 int main(int ac,char *av[])
 {
 	Default_INT46H_Handler=_dos_getvect(FDC_INT);
 	_dos_setvect(FDC_INT,Handle_INT46H);
+	signal(SIGINT,CtrlC);
+
+
+
 	return 0;
 }
