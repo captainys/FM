b02b6c1ff92c6a8eff8ba0eebd95656481e95a88
Soji Yamakawa <soji@andrew.cmu.edu>
2023-09-01 13:04:03 -0400

    Design Change

--- TOWNS/SNES2TOWNS/SNES2TOWNS.ino
+++ TOWNS/SNES2TOWNS/SNES2TOWNS.ino
@@ -5,9 +5,6 @@
 #define LATCH 18  // A1 for LATCH
 #define DATA 19   // A2 for DATA
 
-#define OUT_BEGIN 2    // Pin 2
-#define NUM_OUTPUT 12  // to Pin 13
-
 // Order SNES game pad sends.
 #define _B 0
 #define _Y 1
@@ -22,6 +19,25 @@
 #define _L 10
 #define _R 11
 
+//                   CPSF     TOWNS 6BTN    TOWNS 2BTN
+#define OUT_P1A 13  // UP          UP            UP
+#define OUT_P1B 12  // R           X             UP
+#define OUT_P2A 11  // DOWN        DOWN          DOWN
+#define OUT_P2B 10  // Y           Y             DOWN
+#define OUT_P3A  9  // LEFT        LEFT          LEFT
+#define OUT_P3B  8  // X           L             LEFT
+#define OUT_P4A  7  // RIGHT       RIGHT         RIGHT
+#define OUT_P4B  6  // SEL         R             RIGHT
+#define OUT_P6A  5  // A           A             A
+#define OUT_P6B  4  // L           A             A
+#define OUT_P7A  3  // B           B             B
+#define OUT_P7B  2  // START       B             B
+
+#define OUT_BEGIN OUT_P7B   // Pin 2
+#define OUT_END OUT_P1A  // to Pin 13
+
+
+
 void setup() {
   pinMode(LATCH, OUTPUT);
   pinMode(CLOCK, OUTPUT);
@@ -30,64 +46,13 @@ void setup() {
   digitalWrite(CLOCK, LOW);
   digitalWrite(LATCH, LOW);
 
-  for (int i = 0; i < NUM_OUTPUT; ++i) {
-    pinMode(OUT_BEGIN + i, OUTPUT);
-    digitalWrite(OUT_BEGIN + i, LOW);
+  for(int i=OUT_BEGIN; i<=OUT_END; ++i) {
+    pinMode(i, OUTPUT);
+    digitalWrite(i, LOW);
   }
 
-  pinMode(AS_CPSF, INPUT);
-  pinMode(AS_FJ6BTN, INPUT);
-}
-
-void ConvertAsTOWNS6ButtonPad(unsigned int readbuf[])
-{
-  if(LOW==readbuf[_START])
-  {
-    readbuf[_LEFT]=LOW;
-    readbuf[_RIGHT]=LOW;
-  }
-  if(LOW==readbuf[_SELECT])
-  {
-    readbuf[_UP]=LOW;
-    readbuf[_DOWN]=LOW;
-  }
-
-  // TOWNS 6 Button Pad button label  A,B,C,X,Y,Z
-  // SNES/CPSF Button label           A,B,X,Y,L,R
-
-  unsigned int CBtn=readbuf[_X];
-  unsigned int XBtn=readbuf[_Y];
-  unsigned int YBtn=readbuf[_L];
-  unsigned int ZBtn=readbuf[_R];
-
-  readbuf[_L]=readbuf[_A];
-  readbuf[_START]=readbuf[_B];
-
-  readbuf[_R]=ZBtn;  // <-> UP
-  readbuf[_Y]=YBtn;  // <-> DOWN
-  readbuf[_X]=XBtn;  // <-> LEFT
-  readbuf[_SELECT]=CBtn; // <-> RIGHT
-}
-
-void ConvertAsTOWNSPad(unsigned int readbuf[])
-{
-  if(LOW==readbuf[_START])
-  {
-    readbuf[_LEFT]=LOW;
-    readbuf[_RIGHT]=LOW;
-  }
-  if(LOW==readbuf[_SELECT])
-  {
-    readbuf[_UP]=LOW;
-    readbuf[_DOWN]=LOW;
-  }
-
-  readbuf[_R]=readbuf[_UP];
-  readbuf[_L]=readbuf[_A];
-  readbuf[_Y]=readbuf[_DOWN];
-  readbuf[_START]=readbuf[_B];
-  readbuf[_X]=readbuf[_LEFT];
-  readbuf[_SELECT]=readbuf[_RIGHT];
+  pinMode(AS_TOWNS2BTN, INPUT);
+  pinMode(AS_TOWNS6BTN, INPUT);
 }
 
 enum
@@ -96,6 +61,20 @@ enum
   MODE_AS_TOWNS6BUTTON,
   MODE_AS_TOWNSPAD,
 };
+
+void DoTownsThings(unsigned int readbuf[])
+{
+  if(LOW==readbuf[_SELECT])
+  {
+    readbuf[_UP]=LOW;
+    readbuf[_DOWN]=LOW;
+  }
+  if(LOW==readbuf[_START])
+  {
+    readbuf[_LEFT]=LOW;
+    readbuf[_RIGHT]=LOW;
+  }
+}
 
 void loop() {
   unsigned int readbuf[NUM_OUTPUT];
@@ -116,18 +95,49 @@ void loop() {
   switch(mode)
   {
   case MODE_AS_CPSF:
+    digitalWrite(OUT_P1A,readbuf[_UP]);
+    digitalWrite(OUT_P1B,readbuf[_R]);
+    digitalWrite(OUT_P2A,readbuf[_DOWN]);
+    digitalWrite(OUT_P2B,readbuf[_Y]);
+    digitalWrite(OUT_P3A,readbuf[_LEFT]);
+    digitalWrite(OUT_P3B,readbuf[_X]);
+    digitalWrite(OUT_P4A,readbuf[_RIGHT]);
+    digitalWrite(OUT_P4B,readbuf[_SELECT]);
+    digitalWrite(OUT_P6A,readbuf[_A]);
+    digitalWrite(OUT_P6B,readbuf[_L]);
+    digitalWrite(OUT_P7A,readbuf[_B]);
+    digitalWrite(OUT_P7B,readbuf[_START]);
     break;
   case MODE_AS_TOWNS6BUTTON:
-    ConvertAsTOWNS6ButtonPad(readbuf);
+    DoTownsThings(readbuf);
+    digitalWrite(OUT_P1A,readbuf[_UP]);
+    digitalWrite(OUT_P1B,readbuf[_X]);
+    digitalWrite(OUT_P2A,readbuf[_DOWN]);
+    digitalWrite(OUT_P2B,readbuf[_Y]);
+    digitalWrite(OUT_P3A,readbuf[_LEFT]);
+    digitalWrite(OUT_P3B,readbuf[_L]);
+    digitalWrite(OUT_P4A,readbuf[_RIGHT]);
+    digitalWrite(OUT_P4B,readbuf[_R]);
+    digitalWrite(OUT_P6A,readbuf[_A]);
+    digitalWrite(OUT_P6B,readbuf[_A]);
+    digitalWrite(OUT_P7A,readbuf[_B]);
+    digitalWrite(OUT_P7B,readbuf[_B]);
     break;
   case MODE_AS_TOWNSPAD:
-    ConvertAsTOWNSPad(readbuf);
+    DoTownsThings(readbuf);
+    digitalWrite(OUT_P1A,readbuf[_UP]);
+    digitalWrite(OUT_P1B,readbuf[_UP]);
+    digitalWrite(OUT_P2A,readbuf[_DOWN]);
+    digitalWrite(OUT_P2B,readbuf[_DOWN]);
+    digitalWrite(OUT_P3A,readbuf[_LEFT]);
+    digitalWrite(OUT_P3B,readbuf[_LEFT]);
+    digitalWrite(OUT_P4A,readbuf[_RIGHT]);
+    digitalWrite(OUT_P4B,readbuf[_RIGHT]);
+    digitalWrite(OUT_P6A,readbuf[_A]);
+    digitalWrite(OUT_P6B,readbuf[_A]);
+    digitalWrite(OUT_P7A,readbuf[_B]);
+    digitalWrite(OUT_P7B,readbuf[_B]);
     break;
-  }
-
-  for(int i=0; i<NUM_OUTPUT; ++i)
-  {
-    digitalWrite(OUT_BEGIN + i, readbuf[i]);
   }
 
  /* static int k=OUT_BEGIN;
