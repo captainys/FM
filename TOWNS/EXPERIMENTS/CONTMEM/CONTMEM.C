17d73861594cc1a991eb9f0cd64490f5d670234c
Soji Yamakawa <soji@andrew.cmu.edu>
2023-11-13 07:55:46 -0500

    Exploring possibility of FDDUMP.EXP

--- TOWNS/EXPERIMENTS/CONTMEM/CONTMEM.C
+++ TOWNS/EXPERIMENTS/CONTMEM/CONTMEM.C
@@ -3,10 +3,10 @@
 
 #define PAGE_SIZE 0x1000
 
-#define DATABUF_SIZE (68*1024)
+#define DATABUF_SIZE (128*1024)
 unsigned char _databuf[DATABUF_SIZE];
 
-#define NUM_PHYSADDR (DATABUF_SIZE/4096)
+#define NUM_PHYSADDR ((DATABUF_SIZE+PAGE_SIZE-1)/PAGE_SIZE)
 unsigned int numPhysAddr=0;
 unsigned int physAddr[NUM_PHYSADDR];
 
@@ -24,11 +24,27 @@ struct bufferInfo
 	unsigned int length;
 };
 
+/*
+This function finds physical addresses of the pages within _databuf, sort them, and find sequence of pages that
+are continuous in the physical memory space.  The pages are added to the end of the data segment, and the pointer
+is returned.
+
+The length is up to 128KB, but most likely shorter.  The return value contains pointer to the physically-continuous
+memory space, physical address, and the length.
+
+*/
 struct bufferInfo MakeDataBuffer(void)
 {
+	for(int i=0; i<DATABUF_SIZE; ++i)
+	{
+		_databuf[i]=0x77;
+	}
+
 	struct bufferInfo info;
 	info.ptr=NULL;
 	info.length=0;
+
+	printf("Looking for physically-continuous memory space....\n");
 
 	// Convert pointer to physical address.
 	// Limit until PAGE_SIZE before the end of the array to make sure it gets continuous 4KB if low 11bits are all zero.
@@ -38,9 +54,15 @@ struct bufferInfo MakeDataBuffer(void)
 		unsigned int phys=ToPhysicalAddr(_databuf+i);
 		if(0==(phys&(PAGE_SIZE-1)) && numPhysAddr<NUM_PHYSADDR)
 		{
-			physAddr[numPhysAddr]=phys;
+			printf("%08x ",phys);
+			physAddr[numPhysAddr++]=phys;
+			if(0==(numPhysAddr&7))
+			{
+				printf("\n");
+			}
 		}
 	}
+	printf("\n");
 
 	// Sort page-top physical addresses.
 	for(i=0; i<numPhysAddr; ++i)
@@ -55,6 +77,16 @@ struct bufferInfo MakeDataBuffer(void)
 			}
 		}
 	}
+	printf("Sorted\n");
+	for(i=0; i<numPhysAddr; ++i)
+	{
+		printf("%08x ",physAddr[i]);
+		if(0==(i&7))
+		{
+			printf("\n");
+		}
+	}
+	printf("\n");
 
 	// Find maximum continuous physical addresses.
 	unsigned int maxExtend=0,baseIndex=0;
@@ -74,6 +106,9 @@ struct bufferInfo MakeDataBuffer(void)
 			maxExtend=j-i;
 		}
 	}
+	printf("Longest continuous physical memory space starts at:\n");
+	printf("  Physical Address: %08xH\n",physAddr[baseIndex]);
+	printf("  Length:           %dKB\n",(PAGE_SIZE/4)*maxExtend);
 
 	// Map to the end of DS
 	unsigned char *prevMapped=NULL;
@@ -98,6 +133,7 @@ struct bufferInfo MakeDataBuffer(void)
 
 	info.length=maxExtend*PAGE_SIZE;
 
+	printf("Verify really continuous.\n");
 	// Verify
 	unsigned int prevPhys;
 	for(int i=0; i<info.length; ++i)
@@ -109,8 +145,49 @@ struct bufferInfo MakeDataBuffer(void)
 			exit(1);
 		}
 		prevPhys=phys;
+
+		info.ptr[i]=0xFF;
+		if(0xFF!=info.ptr[i])
+		{
+			printf("Write error to the mapped address.\n");
+			exit(1);
+		}
+		info.ptr[i]=0x00;
+		if(0x00!=info.ptr[i])
+		{
+			printf("Write error to the mapped address.\n");
+			exit(1);
+		}
 	}
 
+	for(i=0; i<DATABUF_SIZE; ++i)
+	{
+		_databuf[i]=0xFF;
+	}
+	for(int i=0; i<info.length; ++i)
+	{
+		if(0xFF!=info.ptr[i])
+		{
+			printf("Mapping error.\n");
+			exit(1);
+		}
+	}
+	for(i=0; i<DATABUF_SIZE; ++i)
+	{
+		_databuf[i]=0x00;
+	}
+	for(int i=0; i<info.length; ++i)
+	{
+		if(0x00!=info.ptr[i])
+		{
+			printf("Mapping error.\n");
+			exit(1);
+		}
+	}
+
+
+
+	printf("Verified.\n");
 	return info;
 }
 
