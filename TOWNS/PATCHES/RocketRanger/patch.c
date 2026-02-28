eb122e212aaa3b7b9ff9b0bc82dcfa5f3dff106b
Soji Yamakawa <soji@andrew.cmu.edu>
2026-02-28 14:56:42 -0500

    Complete Rocket Ranger patch.

--- TOWNS/PATCHES/RocketRanger/patch.c
+++ TOWNS/PATCHES/RocketRanger/patch.c
@@ -78,8 +78,165 @@ unsigned char BGMFileReader_to[]=
 0x9D,0xC3,0x3E,0x80,0x3D,0x4D,0xC1,0x0D,0x00,0xFF,0x75,0xF6,0xE9,0x73,0xFB,0xFF,0xFF
 };
 
+////////////////////////////////////////////////////////////////////////////////////////
 
 
+// Ft.Dix BGM Fix
+// Amiga version plays BGM every time Rocket Ranger returns to the base, Ft. Dix, where FM TOWNS version plays this music
+// only the first time.  This patch fixes the problem.
+//From:
+//000C:0000402B E954FFFFFF                JMP     00003F84 { Chapter1_Loop(Prob):} 
+//To:
+//              E939FFFFFF                JMP     00003F69
+unsigned char FtDixBGM_from[]=
+{
+0xC6,0x05,0x01,0x0D,0x0E,0x00,0x01,0x80,0x3D,0x01,0x0D,0x0E,0x00,0x01,0x0F,0x84,0x2E,0x48,0x02,0x00,0xE9,0x54,0xFF,0xFF,0xFF
+};
+
+unsigned char FtDixBGM_to[]=
+{
+0xC6,0x05,0x01,0x0D,0x0E,0x00,0x01,0x80,0x3D,0x01,0x0D,0x0E,0x00,0x01,0x0F,0x84,0x2E,0x48,0x02,0x00,0xE9,0x39,0xFF,0xFF,0xFF
+};
+
+
+
+////////////////////////////////////////////////////////////////////////////////////////
+
+// Last Boss Background.
+// Amiga version has concentric circles as a background and makes a visual effect like you are travelling in the time machine,
+// where FM TOWNS version is just black background.
+// In fact, FM TOWNS version has a bitmap data for the concentric circles.  Although it does not do animation, I can at least
+// bring it back.
+
+// First, do not draw the bottom half of the screen at the beginning of the last-boss battle.
+// It should be done during the frame-buffer rendering.
+// 000C:300D6 needs to be NOPped.
+// 000C:000300D1 E8B143FDFF                CALL    00004487 { Drawing Upper Half of the Spiral.()}
+// 000C:000300D6 E806000000                CALL    000300E1 { (Prob)Drawing Lower Half of the Spiral by symmetry()}
+// 000C:000300DB E834000000                CALL    00030114
+// 000C:000300E0 C3                        RET
+unsigned char lastBossBackground_1_from[]=
+{
+	0xE8,0xB1,0x43,0xFD,0xFF,0xE8,0x06,0x00,0x00,0x00,0xE8,0x34,0x00,0x00,0x00,0xC3
+};
+unsigned char lastBossBackground_1_to[]=
+{
+	0xE8,0xB1,0x43,0xFD,0xFF,0x90,0x90,0x90,0x90,0x90,0xE8,0x34,0x00,0x00,0x00,0xC3
+};
+
+// Then make "copy upper-half to lower-half" function to take ESI as the source VRAM address.  The address was hard coded.
+// From:
+//	000C:000300EB BE20000000                MOV     ESI,00000020H
+//	000C:000300F0 BF201C0300                MOV     EDI,00031C20H
+// To:
+//                                          LEA EDI,[ESI+31C00H], NOP NOP NOP NOP
+unsigned char lastBossBackground_2_from[]=
+{
+0x66,0xB8,0x0C,0x01,0x8E,0xD8,0x8E,0xC0,0xBE,0x20,0x00,0x00,0x00,0xBF,0x20,0x1C,0x03,0x00,0xB9,0x64,0x00,0x00,0x00
+};
+unsigned char lastBossBackground_2_to[]=
+{
+0x66,0xB8,0x0C,0x01,0x8E,0xD8,0x8E,0xC0,0x8D,0xBE,0x00,0x1C,0x03,0x00,0x90,0x90,0x90,0x90,0xB9,0x64,0x00,0x00,0x00
+};
+
+// Instead of clearing the frame buffer, draw the concentric circles.
+// Clear Frame Buffer()
+// 000C:00030147 BF20000000                MOV     EDI,00000020H
+// 000C:0003014C A048790300                MOV     AL,[00037948H]
+// 000C:00030151 2401                      AND     AL,01H
+// 000C:00030153 0F8405000000              JE      0003015E
+// 000C:00030159 BF00820400                MOV     EDI,00048200H
+// 000C:0003015E 06                        PUSH    ES                   -> PUSH	EDI
+// 000C:0003015F 66B80C01                  MOV     AX,010CH                CALL	000300B6H
+// 000C:00030163 8EC0                      MOV     ES,AX                   POP		ESI
+// 000C:00030165 B9C8000000                MOV     ECX,000000C8H           CALL	000300E1H
+// 000C:0003016A 51                        PUSH    ECX                     RET
+// 000C:0003016B B950000000                MOV     ECX,00000050H           
+unsigned char lastBossBackground_3_from[]=
+{
+0x24,0x01,0x0F,0x84,0x05,0x00,0x00,0x00,0xBF,0x00,0x82,0x04,0x00,0x06,0x66,0xB8,0x0C,0x01,0x8E,0xC0,0xB9,0xC8,0x00,0x00,0x00,0x51
+};
+unsigned char lastBossBackground_3_to[]=
+{
+0x24,0x01,0x0F,0x84,0x05,0x00,0x00,0x00,0xBF,0x00,0x82,0x04,0x00,0x57,0xE8,0x52,0xFF,0xFF,0xFF,0x5E,0xE8,0x77,0xFF,0xFF,0xFF,0xC3
+};
+
+// Boss rendering routine is using three different bit blit functions.  Two of them ignore transparent pixels.
+// Those need to be replaced the one that takes transparency into account.
+//000C:0002FF5C 66A3DC780300              MOV     [000378DCH],AX
+//000C:0002FF62 8A4328                    MOV     AL,[EBX+28H]
+//000C:0002FF65 B400                      MOV     AH,00H
+//000C:0002FF67 66A359790300              MOV     [00037959H],AX
+//000C:0002FF6D E81545FDFF                CALL    00004487     ->   JMP 2FEFB
+//000C:0002FF72 C3                        RET
+unsigned char lastBossBackground_4_from[]=
+{
+0x66,0xA3,0xDC,0x78,0x03,0x00,0x8A,0x43,0x28,0xB4,0x00,0x66,0xA3,0x59,0x79,0x03,0x00,0xE8,0x15,0x45,0xFD,0xFF,0xC3
+};
+unsigned char lastBossBackground_4_to[]=
+{
+0x66,0xA3,0xDC,0x78,0x03,0x00,0x8A,0x43,0x28,0xB4,0x00,0x66,0xA3,0x59,0x79,0x03,0x00,0xe9,0x89,0xff,0xff,0xff,0xC3
+};
+
+//000C:0002FCE2 66A1F6ED0200              MOV     AX,[0002EDF6H]
+//000C:0002FCE8 6605A200                  ADD     AX,00A2H
+//000C:0002FCEC 66A3DA780300              MOV     [000378DAH],AX
+//000C:0002FCF2 66A1FAED0200              MOV     AX,[0002EDFAH]
+//000C:0002FCF8 66056700                  ADD     AX,0067H
+//000C:0002FCFC 66A3DC780300              MOV     [000378DCH],AX
+//000C:0002FD02 66C705597903000C00        MOV     WORD PTR [00037959H],000CH
+//000C:0002FD0B E87747FDFF                CALL    00004487         ->     JMP 4512
+//000C:0002FD10 C3                        RET
+unsigned char lastBossBackground_5_from[]=
+{
+0x66,0xA1,0xF6,0xED,0x02,0x00,0x66,0x05,0xA2,0x00,0x66,0xA3,0xDA,0x78,0x03,0x00,
+0x66,0xA1,0xFA,0xED,0x02,0x00,0x66,0x05,0x67,0x00,0x66,0xA3,0xDC,0x78,0x03,0x00,
+0x66,0xC7,0x05,0x59,0x79,0x03,0x00,0x0C,0x00,0xE8,0x77,0x47,0xFD,0xFF,0xC3
+};
+unsigned char lastBossBackground_5_to[]=
+{
+0x66,0xA1,0xF6,0xED,0x02,0x00,0x66,0x05,0xA2,0x00,0x66,0xA3,0xDA,0x78,0x03,0x00,
+0x66,0xA1,0xFA,0xED,0x02,0x00,0x66,0x05,0x67,0x00,0x66,0xA3,0xDC,0x78,0x03,0x00,
+0x66,0xC7,0x05,0x59,0x79,0x03,0x00,0x0C,0x00,0xE9,0x02,0x48,0xFD,0xFF,0xC3
+};
+
+
+
+// Interrogation scene bug fix.
+// If you make wrong choices, Colonel Leermeister will torture you, and Jane and the Doctor will be taken to somewhere unknown.
+// You eventually escapes from the prisoner camp, and somehow get your rocket pack back....
+// But, FM TOWNS version skips all this storyline message due to a bug.
+// You are suddenly taken to the scene in which you can either select next destination or send an SOS message.
+// At 000C:00019AE0, it sets EAX=the message number, but the programmer forgot calling the fundtion that shows the storyline message.
+// I can fix this bug by utilizing some bytes that are no longer used after above patches.
+//	000C:00019AE0 B836000000                MOV     EAX,00000036H ->   E886660100 CALL 3016BH
+//	000C:00019AE5 8A0DA9090E00              MOV     CL,[000E09A9H]
+//	000C:00019AEB E8BDBEFFFF                CALL    000159AD
+//	000C:00019AF0 A2A9090E00                MOV     [000E09A9H],AL
+//	000C:00019AF5 C605030D0E000B            MOV     BYTE PTR [000E0D03H],0BH   s‚«æ‘I‘ð/SOSB
+//	000C:00019AFC C3                        RET
+unsigned char interrogation_scene_bugfix_1_from[]=
+{
+0xB8,0x36,0x00,0x00,0x00,0x8A,0x0D,0xA9,0x09,0x0E,0x00,0xE8,0xBD,0xBE,0xFF,0xFF,0xA2,0xA9,0x09,0x0E,0x00,0xC6,0x05,0x03,0x0D,0x0E,0x00,0x0B,0xC3
+};
+unsigned char interrogation_scene_bugfix_1_to[]=
+{
+0xE8,0x86,0x66,0x01,0x00,0x8A,0x0D,0xA9,0x09,0x0E,0x00,0xE8,0xBD,0xBE,0xFF,0xFF,0xA2,0xA9,0x09,0x0E,0x00,0xC6,0x05,0x03,0x0D,0x0E,0x00,0x0B,0xC3
+};
+
+// 000C:0003016B B950000000                MOV     ECX,00000050H           -> MOV EAX,36H
+// 000C:00030170 B800000000                MOV     EAX,00000000H           -> CALL 000098F3H
+// 000C:00030175 F3AB                      REP STOSD                       -> CALL 0001B4F8H
+// 000C:00030177 81C7C0020000              ADD     EDI,000002C0H           -> RET
+// 000C:0003017D 59                        POP     ECX                     
+unsigned char interrogation_scene_bugfix_2_from[]=
+{
+0xB9,0x50,0x00,0x00,0x00,0xB8,0x00,0x00,0x00,0x00,0xF3,0xAB,0x81,0xC7,0xC0,0x02,0x00,0x00,0x59
+};
+unsigned char interrogation_scene_bugfix_2_to[]=
+{
+0xB8,0x36,0x00,0x00,0x00,0xE8,0x7E,0x97,0xFD,0xFF,0xE8,0x7E,0xB3,0xFE,0xFF,0xC3,0x90,0x90,0x90
+};
 
 int ApplyPatch(
     int nByte,unsigned char byteData[],
@@ -147,14 +304,22 @@ int ApplyPatchExternal(
 	return 1;
 }
 
+#define numPatterns 10
+
 int main(int ac,char *av[])
 {
 	if(2!=ac)
 	{
 		fprintf(stderr,"Usage: Patch source-file.bin/mdf/iso\n");
 		fprintf(stderr,"  This program applies patches to Rocket Ranger for FM TOWNS\n");
-		fprintf(stderr,"  ISO Image, and correct Opening Demo BGM bug that crashes in\n");
-		fprintf(stderr,"  the FAST mode.\n");
+		fprintf(stderr,"  ISO Image, and...\n");
+		fprintf(stderr,"  (1) Corrects Opening Demo BGM bug that crashes in the FAST mode.\n");
+		fprintf(stderr,"  (2) Plays Ft.Dix theme BGM every time Rocket Ranger comes back to base.\n");
+		fprintf(stderr,"  (3) Draws background picture during the last-boss battle.\n");
+		fprintf(stderr,"  (4) In the Interrogation scene, the programmer apparently forgot to call\n");
+		fprintf(stderr,"      a function to show storyline message.  If you make wrong choices,\n");
+		fprintf(stderr,"      you are taken to the 'Select Destination / Send SOS signal' screen\n");
+		fprintf(stderr,"      with no explanation.  This patch will fix this issue.\n");
 		return 1;
 	}
 
@@ -169,18 +334,30 @@ int main(int ac,char *av[])
 		return 1;
 	}
 
+	unsigned int applyCount[numPatterns]={0};
+
 	unsigned int LBA=0;
 	unsigned char buf[2352];
 	while(2352==fread(buf,1,2352,fp))
 	{
-		int applied[2]=
+		int applied[numPatterns]=
 		{
 			ApplyPatch(2352,buf,sizeof(CALLE660_from),CALLE660_from,sizeof(CALLEADE_to),CALLEADE_to),
 			ApplyPatch(2352,buf,sizeof(BGMFileReader_from),BGMFileReader_from,sizeof(BGMFileReader_to),BGMFileReader_to),
+			ApplyPatch(2352,buf,sizeof(FtDixBGM_from),FtDixBGM_from,sizeof(FtDixBGM_to),FtDixBGM_to),
+			ApplyPatch(2352,buf,sizeof(lastBossBackground_1_from),lastBossBackground_1_from,sizeof(lastBossBackground_1_to),lastBossBackground_1_to),
+			ApplyPatch(2352,buf,sizeof(lastBossBackground_2_from),lastBossBackground_2_from,sizeof(lastBossBackground_2_to),lastBossBackground_2_to),
+			ApplyPatch(2352,buf,sizeof(lastBossBackground_3_from),lastBossBackground_3_from,sizeof(lastBossBackground_3_to),lastBossBackground_3_to),
+			ApplyPatch(2352,buf,sizeof(lastBossBackground_4_from),lastBossBackground_4_from,sizeof(lastBossBackground_4_to),lastBossBackground_4_to),
+			ApplyPatch(2352,buf,sizeof(lastBossBackground_5_from),lastBossBackground_5_from,sizeof(lastBossBackground_5_to),lastBossBackground_5_to),
+			ApplyPatch(2352,buf,sizeof(interrogation_scene_bugfix_1_from),interrogation_scene_bugfix_1_from,sizeof(interrogation_scene_bugfix_1_to),interrogation_scene_bugfix_1_to),
+			ApplyPatch(2352,buf,sizeof(interrogation_scene_bugfix_2_from),interrogation_scene_bugfix_2_from,sizeof(interrogation_scene_bugfix_2_to),interrogation_scene_bugfix_2_to),
+			// ApplyPatch(2352,buf,sizeof(xxxx_from),xxxx_from,sizeof(xxxx_to),xxxx_to),
 		};
 
-		for(int i=0; i<sizeof(applied)/sizeof(int); ++i)
+		for(int i=0; i<numPatterns; ++i)
 		{
+			applyCount[i]+=applied[i];
 			if(0!=applied[i])
 			{
 				printf("Applied Patch %d to LBA=%d\n",i,LBA);
@@ -195,6 +372,18 @@ int main(int ac,char *av[])
 	}
 
 	fclose(fp);
+
+	for(int i=0; i<numPatterns; ++i)
+	{
+		if(0==applyCount[i])
+		{
+			printf("Pattern %d is not applied.  The patched disc may not work.\n",i);
+		}
+		if(1<applyCount[i])
+		{
+			printf("Pattern %d is applied more than once (%d times).  The patched disc may not work.\n",i,applyCount[i]);
+		}
+	}
 
 	printf("Patched!\n");
 	return 0;
--- TOWNS/PATCHES/RocketRanger/readme.txt
+++ TOWNS/PATCHES/RocketRanger/readme.txt
@@ -5,6 +5,9 @@ Introduction
 
 ロケットレンジャーは、ゲーム史に名を残すべき傑作ゲームであり、FM TOWNSに移植されたことはFM TOWNSユーザーとしては非常に喜ぶべきことだと思ってますが、残念ながらFM TOWNS版はバグってます。
 
+
+
+1. 高速モードだとオープニングでクラッシュする。
 高速モードでロケットレンジャーを起動した場合、オープニングBGMに対して動画が速く進み過ぎる結果、まだ最初のBGMが終わってないのに、メインテーマのBGMをロードしようとしてクラッシュします。
 
 ところが、クラッシュに寄ってPICのIn-Service Registerが立ったままの状態になるため、奇跡的にBGM無しでゲームは進行します。
@@ -17,11 +20,43 @@ Introduction
 
 だから、他にもFM TOWNS II HR以降を使ってた人でロケットレンジャーやったけどBGMの存在を知らないままだったという人がいるかもしれません。そういう人は、今からでもロケットレンジャーのCDイメージにこのパッチを適用してFM TOWNS実機あるいはFM TOWNSエミュレータ津軽を使ってプレイしてみましょう。このパッチを当てると高速モードでも上の問題が発生しないので、非常に印象的なBGMを聞くことができます。
 
+
+
+2. Fort DixのBGMが初回しか鳴らない
+Amiga版だと、Fort Dixに戻るたびにFort Dixのテーマ曲が流れるのですが、TOWNS版はなぜかゲームを起動してオープニングが終わった直後の一度しか鳴りません。このパッチを当てると、Fort Dixに戻るたびにテーマ曲が流れるようになります。
+
+
+
+3. ラスボス戦の背景が無い
+Amiga版では、ラスボス戦で、背景にタイムトンネルみたいなのが表示されますが、TOWNS版だとただの黒い背景です。
+
+実はTOWNS版にもタイムトンネルみたいな背景画像はあって、ラスボス戦開始直後の1フレームだけ見えます。
+
+このパッチを当てると、ラスボス戦の背景が見えるようになります。残念ながらAmiga版のようなパレットアニメーションはありません。
+
+
+
+4. ジェーンと博士を救出に向かって自分も捕まったシーン
+尋問を受けるシーンで、選択肢を間違えると、何の説明もないまま、目的地選択 or SOS信号発信画面に飛ばされます。捕まって尋問を受けてたはずなのに、普通にロケットパックで次の目的地に飛べるのは明らかにおかしいバグです。
+
+この途中に何があったのか、そのストーリーを表示するテキストがなぜか表示されないというバグがあります。どうも、メッセージIDだけEAXレジスタにセットして、メッセージを表示する関数を呼ぶのを忘れたようです。
+
+このパッチにより、その場面のストーリーのテキストを読めるようになります。
+
+ただ、テキストを見ても、捕虜収容所を脱走したまではいいとして、どうやってロケットパックを奪還したのかの説明が無いので、ここはちょっとストーリーの連続性が欠けてるんですが。
+
+
+
+
+
 でも、このゲーム離陸が難し過ぎたよね。
 
 
 
 Rocket Ranger is one of the games that should be permanently remembered as a game that impacted the history of the video game.  And, it was very lucky for FM TOWNS users that the game was available for the platform.  However, FM TOWNS port was bugged.
+
+
+1. Opening DEMO Crashes if you start in the FAST mode.
 
 If you start Rocket Ranger in the FAST mode, the opening movie runs too fast relative to the BGM.  Then, the program tries to load the main-theme music while the intro BGM is still playing, and the program crashes.
 
@@ -33,7 +68,43 @@ One of the reasons why Rocket Ranger should be remembered is its superb music.  
 
 Maybe other FM TOWNS users did not know there Rocket Ranger had a music.  If you are one of such FM TOWNS and Rocket Ranger users, try this patch, and play again on the real hardware or FM TOWNS emulator Tsugaru.  Now you can listen to one of the greates game music in history.
 
-But, takeoff was too difficult.
+
+
+2. Fort Dix BGM plays only once after the opening demo.
+
+In Amiga version, it plays Fort Dix theme BGM every time you come back to Fort Dix.  But, FM TOWNS version plays only once after the opening demo.
+
+You will hear Fort Dix BGM every time you come back to the home base after this patch.
+
+
+
+3. Interrogation scene bug
+
+When you fly for rescue Jane and the doctor and be captured.  You will be interrogated by Colonel Leermeister.  If you make wrong choices, you are tortured and then taken to the prisoner camp, and you eventually escape.
+
+But, in FM TOWNS version, if you make wrong choices, you are suddenly taken to the screen in which you can choose the next destination, or you can send SOS signal.  What's the F**K?  I was in the interrogation room, and all of a sudden, I was free!?  What happened to me?
+
+There is a text explaining what happened in the code, but the programmer who ported it to FM TOWNS was setting the message ID 00000036H in EAX register but apparently forgot calling a function to show it on the screen.
+
+This patch will fix this problem.
+
+But, even the text did not explain how you got back your rocket pack.  There was a discontinuity in the game scenario here anyway.
+
+
+
+4. Background during the last-boss battle.
+
+The Amiga version had a background like a time tunnel during the last-boss battle.  FM TOWNS version had no background.
+
+But, FM TOWNS version had a bitmap pattern of the background.  It is shown only for the one frame at the beginning of the last-boss battle.
+
+This patch will make it visible during the last-boss battle.  Unfortunately it does not do palette animation like the Amiga version.
+
+
+
+
+
+But, takeoff was too difficult in this game....
 
 
 
